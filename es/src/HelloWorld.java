17fb569f4e7cd5aafb6be29fdb7630911244222b
fangpengbo <17630838399@yeah.net>
2022-08-29 22:56:04 +0800

    [es]1.init

---
+++ es/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Fang Pengbo
+ * @Date: 2022/08/29/22:55
+ * @Description:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    }
+
+}
