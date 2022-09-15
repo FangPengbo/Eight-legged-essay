fac82a7fc7bb52e7a01d139473a6e6b73d727b59
fangpengbo <17630838399@yeah.net>
2022-09-16 00:10:07 +0800

    [mq]1.mq面试题

---
+++ mq/src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Fang Pengbo
+ * @Date: 2022/09/16/0:06
+ * @Description:
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+    }
+
+}
